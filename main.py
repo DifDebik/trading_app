e9372b7c1b17bd479adc0ff900272af96cfb33cd
Dif <zzeni42@gmail.com>
2024-08-26 17:38:22 +0300

    validation

--- main.py
+++ main.py
@@ -1,19 +1,38 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, status
+from fastapi.encoders import jsonable_encoder
+from fastapi.exceptions import ValidationException
+from fastapi.responses import JSONResponse
+
+from typing import List
+from validation import Trade, User
+
 
 app = FastAPI(
     title='Trading App'
 )
 
 
+@app.exception_handler(ValidationException)
+async def validation_exception_handler(request: Request,
+                                       exc: ValidationException):
+    return JSONResponse(
+        status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+        content=jsonable_encoder({"detail": exc.errors()}),
+    )
+
+
 fake_users = [
     {"id": 1, "role": "admin", "name": "Bob"},
     {"id": 2, "role": "investor", "name": "John"},
     {"id": 3, "role": "trader", "name": "Matt"},
+    {"id": 4, "role": "investor", "name": "Homer", "degree": [
+        {"id": 1, "created_at": "2020-01-01T00:00:00", "type_degree": "expert"}
+    ]},
 ]
 
 
-@app.get('/users/{user_id}')
-def get_hello(user_id: int):
+@app.get('/users/{user_id}', response_model=List[User])
+def get_user(user_id: int):
     return [user for user in fake_users if user.get('id') == user_id]
 
 
@@ -21,11 +40,6 @@ fake_trades = [
     {"id": 1, "user_id": 1, "currency": "BTC", "side": "buy", "price": 123, "amount": 2.12},
     {"id": 2, "user_id": 1, "currency": "BTC", "side": "sell", "price": 125, "amount": 2.12},
 ]
-
-
-@app.get('/trades')
-def get_trades(limit: int = 1, offset: int = 0):
-    return fake_trades[offset:][:limit]
 
 
 fake_users2 = [
@@ -40,3 +54,9 @@ def change_user_name(user_id: int, new_name: str):
     current_user = list(filter(lambda user: user.get('id') == user_id, fake_users2))[0]
     current_user['name'] = new_name
     return {'status': 200, 'data': current_user}
+
+
+@app.post('/trades')
+def add_trades(trades: List[Trade]):
+    fake_trades.extend(trades)
+    return {'status': 200, 'data': fake_trades}
